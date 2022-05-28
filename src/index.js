defd993a96ae6bf4512689f40e9aaaa1fc42244a
jm8084 <jm8084@rit.edu>
2022-05-27 20:18:02 -0400

    many design changes & pivots; ready for early release...I think

--- src/index.js
+++ src/index.js
@@ -34,19 +34,20 @@ function showPage(page, button) {
 
 /**
  * Moves element right 1px until end of doc; then resets from left
- * @param {element passed to flow for animation} elem 
+ * @param {element to be affected by flow animation} elem 
  */
 function flow(elem) {
     const maxWidth = elem.parentElement.clientWidth;
     var pos = elem.offsetLeft;
     const id = setInterval(()=>{ 
+        if(local.getItem('page') === 'homePage'){
         // opposite flow process for 'midRow' txt class 
-        if(elem.parentElement.className.includes('midRow')){
+         if (elem.parentElement.className.includes('midRow')){
             // check end border reached to reset else move on
             if(pos == -(elem.clientWidth)){
                 pos = maxWidth;
                 elem.style.left = maxWidth;
-                elem.style.marginTop = Math.floor(Math.random() * 15) + '%';
+                elem.style.marginTop = Math.floor(Math.random() * 45) + 3 + '%';
             } else {
                 pos--;
                 elem.style.left = pos + 'px';
@@ -56,20 +57,51 @@ function flow(elem) {
             if(pos == maxWidth){
                 pos = -(elem.clientWidth);
                 elem.style.left = -(elem.clientWidth) + 'px';
-                elem.style.marginTop = Math.floor(Math.random() * 10) + '%';
+                elem.style.marginTop = Math.floor(Math.random() * 45) + 3 + '%';
             } else {
                 pos++;
                 elem.style.left = pos + 'px';
             }
-    }
-    }, 1);
-    
+        }
+        
+    }}, 1);  
 }
+
+/**
+ * 
+ * @param {topic name(key) to get data to be displayed on the about page} topic 
+ */
+function aboutInfo(topic) {
+    topics.forEach(arr => {
+        if(arr.name == topic){
+            //update inner html
+            console.log(arr.description);
+        } else {
+            console.log('data not found!');
+        }
+    });
+}
+
+// page data for aboutPage topics
+const topics = [
+    {
+        "name":"dog",
+        "description": "Love my DOGO!"
+    },
+    {
+        "name":"car",
+        "description": "Love my WHIP!"
+    },
+    {
+        "name":"stock",
+        "description": "Love my STONKS!"
+    }
+]
 
 // init
 const local = window.localStorage;
 const cHeader = "rgb(44, 52, 95)";
-local.setItem('page', 'homePage');
+local.setItem('page', 'homePage');  //pages: homePage, aboutPage, projectsPage
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('button').forEach(button => {
@@ -80,6 +112,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelectorAll('.txt').forEach(elem => {
         flow(elem);
-    })
+    });
+});
+
+document.addEventListener('click', () => {
+    document.querySelectorAll('.aboutTopic').forEach(topic => {
+        aboutInfo(topic.dataset.topic);
+    });
 });
 
