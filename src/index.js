92b2ff4b05a6f7040f181ff5999c0bed186aaff7
jm8084 <jm8084@rit.edu>
2022-05-31 11:43:21 -0400

    homePage mobile done; txt flow,rand,& select updated;

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 
 /**
  * Display div(page) elements based on which nav button is selected.
- **/
+ * 
+ * @param {Page to be shown} page 
+ * @param {acive page button} button 
+ */
 function showPage(page, button) {
     // hide all pages from view & show selected page
     document.querySelectorAll('.page-wrapper').forEach(div => {
@@ -34,37 +37,106 @@ function showPage(page, button) {
 
 /**
  * Moves element right 1px until end of doc; then resets from left
+ * 
  * @param {element to be affected by flow animation} elem 
  */
 function flow(elem) {
     const maxWidth = elem.parentElement.clientWidth;
+    const maxHeight = elem.parentElement.clientHeight;
+
+    var rand = getRand(maxHeight);
+    elemHeights[elem.dataset.idx] = rand;
+    elem.style.top = rand+"px";
+
+    // start element on left or right of screen
+    if(elem.dataset.flow == "L"){
+        elem.style.left = maxWidth + "px";
+    } else {
+        elem.style.left = -(elem.clientWidth)+"px";
+    }
+
     var pos = elem.offsetLeft;
+
+    const resetElem = (e)=>{
+        setHTML(e);
+        rand = getRand(maxHeight);
+        elemHeights[e.dataset.idx] = rand;
+        e.style.top = rand+"px";
+    }
+
+    setHTML(elem);
+
     const id = setInterval(()=>{ 
         if(local.getItem('page') === 'homePage'){
-        // opposite flow process for 'midRow' txt class 
-         if (elem.parentElement.className.includes('midRow')){
-            // check end border reached to reset else move on
-            if(pos == -(elem.clientWidth)){
-                pos = maxWidth;
-                elem.style.left = maxWidth;
-                elem.style.marginTop = Math.floor(Math.random() * 45) + 3 + '%';
+            if(elem.dataset.flow == "L"){
+                if(pos == -(elem.clientWidth)){
+                    pos = maxWidth;
+                    elem.style.left = maxWidth +"px";
+                    resetElem(elem);
+                } else {
+                    pos--;
+                    elem.style.left = pos + 'px';
+                }
             } else {
-                pos--;
-                elem.style.left = pos + 'px';
-            }
-        } else {
-            // check end border reached to reset else move on
-            if(pos == maxWidth){
-                pos = -(elem.clientWidth);
-                elem.style.left = -(elem.clientWidth) + 'px';
-                elem.style.marginTop = Math.floor(Math.random() * 45) + 3 + '%';
-            } else {
-                pos++;
-                elem.style.left = pos + 'px';
-            }
+                if(pos == maxWidth){
+                    pos = -(elem.clientWidth);
+                    elem.style.left = -(elem.clientWidth) + 'px';
+                    resetElem(elem);
+                } else {
+                    pos++;
+                    elem.style.left = pos + 'px';
+                }
+            }     
         }
-        
-    }}, 1);  
+    }, 20);  
+}
+
+// set innerHTML
+function setHTML(elem){
+    const maxRand = techs.length;
+
+    const avail = techs.map((tech) => {
+        if(!(elemTxt.includes(tech))){
+            return tech;
+        }
+    });   
+
+    var txtSelect = (arr)=>{
+        var x = Math.floor(Math.random() * maxRand);
+        if(arr[x] == undefined){
+            return txtSelect(arr);
+        } else {return arr[x];}
+    }
+
+    const txt = txtSelect(avail);
+    elem.innerHTML = txt;
+    elemTxt[elem.dataset.idx] = txt;
+
+}
+
+/**
+ * Generates a random number thats at least 'padding' pixels away
+ * from preveious numbers (elemHeights) and greater than 50.
+ * 
+ * @param {max distance (in pixels) from top of screen} max
+ * @returns Random number meeting all constraints
+ */
+function getRand(max){
+    const padding = 65;
+    var rand = 0;
+    var valid = false;
+
+    while(!valid){
+        valid = true;
+        rand = Math.floor(Math.random() * (max-65));
+
+        elemHeights.forEach((x)=>{
+            if((Math.abs(x - rand)<=padding) || (rand < 50) ) {
+                valid = false;     
+            }
+        });   
+    }
+    return rand;
 }
 
 /**
@@ -82,6 +154,8 @@ function aboutInfo(topic) {
     });
 }
 
+/**********-------------------- END FUNCTIONS --------------------***********/
+
 // page data for aboutPage topics
 const topics = [
     {
@@ -96,7 +170,16 @@ const topics = [
         "name":"stock",
         "description": "Love my STONKS!"
     }
-]
+];
+
+const techs = [
+    "CSS", "DJango", "Material-UI","Postgres", "AWS",
+    "git", "Python", "Javascript", "Java","Bootstrap",
+    "React.js", "HTML", "MongoDB"
+];
+const elemHeights = [];
+const elemTxt = [" ", " ", " ", " ", " "];
+
 
 // init
 const local = window.localStorage;
@@ -111,7 +194,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.querySelectorAll('.txt').forEach(elem => {
-        flow(elem);
+        setTimeout(flow(elem), Math.floor(Math.random() * 500)+50);
     });
 });
 
